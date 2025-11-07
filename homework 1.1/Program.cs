fad89f869766c0de0eafbd851f49fba309fe7629
Farbod Biparva <farbodbiparvaa@Gmail.com>
2025-11-08 02:02:24 +0330

    Add files via upload

---
+++ homework 1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the radius: ");
+        double r = double.Parse(Console.ReadLine());
+
+        double masahat = 3.14 * r * r;
+        double mohit = 2 * 3.14 * r;
+
+        Console.WriteLine($"masahat: {masahat}");
+        Console.WriteLine($"Circumference: {mohit}");
+    }
+}
