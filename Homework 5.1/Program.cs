fcdccd86f7873af48c1724f3eb66c951aefa26ff
Farbod Biparva <farbodbiparvaa@Gmail.com>
2025-11-18 01:18:06 +0330

    Add files via upload

---
+++ Homework 5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static double Calculatemohit(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    static double CalculateArea(double a, double b, double c)
+    {
+        double s = (a + b + c) / 2; 
+        return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Please Enter sides of your Triangle:");
+
+        Console.Write("side a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("side b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("side c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double mohit = Calculatemohit(a, b, c);
+        double area = CalculateArea(a, b, c);
+
+        Console.WriteLine($"\ntraingle mohit: {mohit}");
+        Console.WriteLine($"triangle area: {area}");
+    }
+}
---
+++ Homework 5.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static int Multiply(int a, int b)
+    {
+
+        if (b == 0)
+            return 0;
+        return a + Multiply(a, b - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("adad aval ra vared konid : ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("adad dovom ra vared konid: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int result = Multiply(num1, num2);
+
+        Console.WriteLine($"\nhasel zarb {num1} va {num2} barabar ast ba: {result}");
+    }
+}
